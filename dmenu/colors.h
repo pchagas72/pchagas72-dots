ec68f06ac851cfd27aaf52286f09125300b8877c
PedroChagas <pchagas.cloud@gmail.com>
2023-02-20 16:49:53 -0300

    Added st, dwm, dmenu and slstatus

---
+++ dmenu/colors.h
@@ -0,0 +1,6 @@
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#9ccbed", "#0B0312" },
+	[SchemeSel] = { "#9ccbed", "#272D70" },
+	[SchemeOut] = { "#9ccbed", "#3497E5" },
+};
