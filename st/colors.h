2f95f0439d711fd89fe0123f6d5578f58ab0d78a
PedroChagas <pchagas.cloud@gmail.com>
2023-02-21 21:48:13 -0300

    Creatd my own ST fork, better than using a ready-made like I was w/ the lukesSmith patch.

--- st/colors.h
+++ st/colors.h
@@ -1,29 +1,29 @@
 const char *colorname[] = {
 
   /* 8 normal colors */
-  [0] = "#0B0312", /* black   */
-  [1] = "#272D70", /* red     */
-  [2] = "#214379", /* green   */
-  [3] = "#25358C", /* yellow  */
-  [4] = "#2A55A2", /* blue    */
-  [5] = "#386FCB", /* magenta */
-  [6] = "#3497E5", /* cyan    */
-  [7] = "#9ccbed", /* white   */
+  [0] = "#0d0c17", /* black   */
+  [1] = "#5C4D69", /* red     */
+  [2] = "#4D4D72", /* green   */
+  [3] = "#8D5973", /* yellow  */
+  [4] = "#69628D", /* blue    */
+  [5] = "#8B76A1", /* magenta */
+  [6] = "#9A84AE", /* cyan    */
+  [7] = "#cdbcd2", /* white   */
 
   /* 8 bright colors */
-  [8]  = "#6d8ea5",  /* black   */
-  [9]  = "#272D70",  /* red     */
-  [10] = "#214379", /* green   */
-  [11] = "#25358C", /* yellow  */
-  [12] = "#2A55A2", /* blue    */
-  [13] = "#386FCB", /* magenta */
-  [14] = "#3497E5", /* cyan    */
-  [15] = "#9ccbed", /* white   */
+  [8]  = "#8f8393",  /* black   */
+  [9]  = "#5C4D69",  /* red     */
+  [10] = "#4D4D72", /* green   */
+  [11] = "#8D5973", /* yellow  */
+  [12] = "#69628D", /* blue    */
+  [13] = "#8B76A1", /* magenta */
+  [14] = "#9A84AE", /* cyan    */
+  [15] = "#cdbcd2", /* white   */
 
   /* special colors */
-  [256] = "#0B0312", /* background */
-  [257] = "#9ccbed", /* foreground */
-  [258] = "#9ccbed",     /* cursor */
+  [256] = "#0d0c17", /* background */
+  [257] = "#cdbcd2", /* foreground */
+  [258] = "#cdbcd2",     /* cursor */
 };
 
 /* Default colors (colorname index)
@@ -31,5 +31,5 @@ const char *colorname[] = {
  unsigned int defaultbg = 0;
  unsigned int defaultfg = 257;
  unsigned int defaultcs = 258;
- unsigned int defaultrcs = 258;
+ unsigned int defaultrcs= 258;
  unsigned int background = 258;
