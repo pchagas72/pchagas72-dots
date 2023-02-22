be2329efd6449993efaa7e917f0d3882d0018733
PedroChagas <pchagas.cloud@gmail.com>
2023-02-21 21:29:31 -0300

    Added colors.h file to st. Kinda useless but I like it that way

---
+++ st/colors.h
@@ -0,0 +1,35 @@
+const char *colorname[] = {
+
+  /* 8 normal colors */
+  [0] = "#0B0312", /* black   */
+  [1] = "#272D70", /* red     */
+  [2] = "#214379", /* green   */
+  [3] = "#25358C", /* yellow  */
+  [4] = "#2A55A2", /* blue    */
+  [5] = "#386FCB", /* magenta */
+  [6] = "#3497E5", /* cyan    */
+  [7] = "#9ccbed", /* white   */
+
+  /* 8 bright colors */
+  [8]  = "#6d8ea5",  /* black   */
+  [9]  = "#272D70",  /* red     */
+  [10] = "#214379", /* green   */
+  [11] = "#25358C", /* yellow  */
+  [12] = "#2A55A2", /* blue    */
+  [13] = "#386FCB", /* magenta */
+  [14] = "#3497E5", /* cyan    */
+  [15] = "#9ccbed", /* white   */
+
+  /* special colors */
+  [256] = "#0B0312", /* background */
+  [257] = "#9ccbed", /* foreground */
+  [258] = "#9ccbed",     /* cursor */
+};
+
+/* Default colors (colorname index)
+ * foreground, background, cursor */
+ unsigned int defaultbg = 0;
+ unsigned int defaultfg = 257;
+ unsigned int defaultcs = 258;
+ unsigned int defaultrcs = 258;
+ unsigned int background = 258;
--- st/config.h
+++ st/config.h
@@ -111,42 +111,7 @@ float alpha = 0.5;
 float alphaOffset = 0.0;
 float alphaUnfocus;
 
-/* Terminal colors (16 first used in escape sequence) */
-static const char *colorname[] = {
-	"#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
-	"#cc241d",
-	"#98971a",
-	"#d79921",
-	"#458588",
-	"#b16286",
-	"#689d6a",
-	"#a89984",
-	"#928374",
-	"#fb4934",
-	"#b8bb26",
-	"#fabd2f",
-	"#83a598",
-	"#d3869b",
-	"#8ec07c",
-	"#ebdbb2",
-	[255] = 0,
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#add8e6", /* 256 -> cursor */
-	"#555555", /* 257 -> rev cursor*/
-	"#282828", /* 258 -> bg */
-	"#ebdbb2", /* 259 -> fg */
-};
-
-
-/*
- * Default colors (colorname index)
- * foreground, background, cursor, reverse cursor
- */
-unsigned int defaultfg = 259;
-unsigned int defaultbg = 258;
-unsigned int defaultcs = 256;
-unsigned int defaultrcs = 257;
-unsigned int background = 258;
+#include "colors.h"
 
 /*
  * Default shape of cursor
