c22a22bb6391fba3c43865292d0704ce9c91257b
PedroChagas <pchagas.cloud@gmail.com>
2023-02-21 21:56:40 -0300

    Fixed flameshot ctrl-c problem

--- dwm/config.h
+++ dwm/config.h
@@ -53,12 +53,13 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-l", "4", NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *flameshotcmd[] = {"flameshot", "gui", NULL};
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY|ShiftMask,             XK_s,      spawn,          SHCMD("flameshot gui") },
+	{ MODKEY|ShiftMask,             XK_s,      spawn,          {.v = flameshotcmd} },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
