f3871220f7a8b0c2db5832bb771386136727028a
Pedro Chagas <pchagas.cloud@gmail.com>
2024-02-22 15:25:41 -0300

    Changed dmenu colors

--- suckless/dmenu/colors.h
+++ suckless/dmenu/colors.h
@@ -1,8 +1,8 @@
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#aabccc", "#08050C" },
-	[SchemeSel] = { "#aabccc", "#4C616F" },
-	[SchemeOut] = { "#aabccc", "#4C67B2" },
-    [SchemeSelHighlight] = { "#023612", "#aabccc" },
-    [SchemeNormHighlight] = { "#022600", "#aabccc" },
+	[SchemeNorm] = { "#aabbcc", "#03041f" },
+	[SchemeSel] = { "#aabbcc", "#03041f" },
+	[SchemeOut] = { "#aabbcc", "#03041f" },
+    [SchemeSelHighlight] = { "#aabbcc", "#274083" },
+    [SchemeNormHighlight] = { "#aabbcc", "#274083" },
 };
