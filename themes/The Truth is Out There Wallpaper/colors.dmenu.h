4a95523ea6f102879fdcfb74b3d8ad7904f68d05
Pedro Chagas <pedrochagas.sm@gmail.com>
2024-05-18 16:44:01 -0300

    Added wayland

--- themes/The Truth is Out There Wallpaper/colors.dmenu.h
+++
@@ -1,8 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#c3c6ca", "#08090C" },
-	[SchemeSel] = { "#c3c6ca", "#48546E" },
-	[SchemeOut] = { "#c3c6ca", "#888F98" },
-        [SchemeSelHighlight] = { "#08090C", "#c3c6ca" },
-        [SchemeNormHighlight] = { "#efdbcd", "#0f0f0d" },
-};
--- themes/retro_green/colors.dmenu.h
+++
@@ -1,8 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#1FC742", "#010101" },
-	[SchemeSel] = { "#1FC742", "#091c0e" },
-	[SchemeOut] = { "#1FC742", "#010101" },
-        [SchemeSelHighlight] = { "#023612", "#1FC742" },
-        [SchemeNormHighlight] = { "#022600", "#1FC742" },
-};
