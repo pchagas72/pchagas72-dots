f08710615b4228296b00965502a7ca7972e55ae2
Pedro Chagas <pedro.pcsilva@ufpe.br>
2025-06-09 13:23:52 -0300

    mid 2025 revamp, created misc folder and deleted old scripts

--- scripts/select_term.sh
+++
@@ -1,9 +0,0 @@
-PROMPT=$(echo -e "alacritty                 \nst" | dmenu -p "select default terminal")
-
-if [ $PROMPT  == "st" ]; then
-	echo "st" > $HOME/.local/utilities/system-vars/default_term
-
-elif [ $PROMPT == "alacritty" ]; then
-	echo "alacritty" > $HOME/.local/utilities/system-vars/default_term
-
-fi
--- scripts/selected_term.sh
+++
@@ -1,7 +0,0 @@
-TERM_LOCATION=$HOME/.local/utilities/system-vars/default_term
-TERM=$(cat $HOME/.local/utilities/system-vars/default_term)
-if [[ "$TERM" == "" ]] ; then
-	$(select_term)
-fi
-$($TERM)
-
